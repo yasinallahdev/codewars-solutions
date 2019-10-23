ab5c016a28029a7691c40a97905a6809b4de090f
yasinallahdev <yasin.allahdev@gmail.com>
2019-10-23 19:18:19 -0400

    Bit Converter C# Codewar

---
+++ C# Codewars/bitconverter.cs
@@ -0,0 +1,40 @@
+/*
+
+    PROBLEM:
+
+    Description:
+    Given an array of ones and zeroes, convert the equivalent binary value to an integer.
+
+    Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
+
+    Examples:
+
+    Testing: [0, 0, 0, 1] ==> 1
+    Testing: [0, 0, 1, 0] ==> 2
+    Testing: [0, 1, 0, 1] ==> 5
+    Testing: [1, 0, 0, 1] ==> 9
+    Testing: [0, 0, 1, 0] ==> 2
+    Testing: [0, 1, 1, 0] ==> 6
+    Testing: [1, 1, 1, 1] ==> 15
+    Testing: [1, 0, 1, 1] ==> 11
+    However, the arrays can have varying lengths, not just limited to 4.
+
+*/
+
+// SOLUTION
+using System;
+
+namespace Solution {
+  class Kata {
+      public static int binaryArrayToNumber(int[] BinaryArray) {
+          int finalNumber = 0;
+          if (BitConverter.IsLittleEndian) {
+            Array.Reverse(BinaryArray);
+          }
+          for(int i = 0;i<BinaryArray.Length;i++) {
+            finalNumber |= (BinaryArray[i] << i);
+          }
+          return finalNumber;
+        }
+    }
+}
