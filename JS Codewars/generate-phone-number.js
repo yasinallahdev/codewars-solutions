788c6f4dbaa072b495e1197c089ffd84e25a45d3
yasinallahdev <yasin.allahdev@gmail.com>
2019-11-11 16:31:45 -0500

    Generate Phone Number (JavaScript)

---
+++ JS Codewars/generate-phone-number.js
@@ -0,0 +1,20 @@
+/*
+
+    PROBLEM:
+
+    Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+
+    Example:
+    createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
+    The returned format must be correct in order to complete this challenge.
+    Don't forget the space after the closing parenthesis!
+
+*/
+
+// SOLUTION
+
+// as the array will always be 10 integers beteen 0 and 9
+function createPhoneNumber(n){
+    // we do not currently validate the input, and simply build the phone number using a template string.
+    return `(${n[0]}${n[1]}${n[2]}) ${n[3]}${n[4]}${n[5]}-${n[6]}${n[7]}${n[8]}${n[9]}`;
+}
